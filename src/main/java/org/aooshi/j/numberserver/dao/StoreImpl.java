5a6af12446d638ac038f719eab3bab9f522ec38f
zhuchangqing <1677221979@qq.com>
2019-10-12 17:14:41 +0800

    sql防注入

--- src/main/java/org/aooshi/j/numberserver/dao/StoreImpl.java
+++ src/main/java/org/aooshi/j/numberserver/dao/StoreImpl.java
@@ -21,8 +21,9 @@ public class StoreImpl implements IStore {
         Date date = new Date();
         int rows = 0;
         try {
-            rows = jdbcTemplate.update("insert into jnumberserver_store(id,v,createtime) values(?,?,?)",
-                    id, value, date);
+            Object[] obj = new Object[]{id, value, date};
+            String sql = "insert into jnumberserver_store(id,v,createtime) values(?,?,?)";
+            rows = jdbcTemplate.update(sql, obj);
         } catch (DuplicateKeyException e) {
             //String message = "id exists";
             return ActionCode.ID_EXISTS;
@@ -36,14 +37,18 @@ public class StoreImpl implements IStore {
 
     @Override
     public int update(String id, Long value) {
-        jdbcTemplate.update("update jnumberserver_store set v=" + value + " where id='" + id + "'");
+        Object[] obj = new Object[]{value, id};
+        String sql = "update jnumberserver_store set v=? where id=?";
+        jdbcTemplate.update(sql, obj);
         return ActionCode.OK;
     }
 
     @Override
     public int delete(String id) {
         int rows = 0;
-        rows = jdbcTemplate.update("delete from jnumberserver_store where id='" + id + "'");
+        Object[] obj = new Object[]{id};
+        String sql = "delete from jnumberserver_store where id=?";
+        rows = jdbcTemplate.update(sql, obj);
         if (rows > 0)
             return ActionCode.OK;
 
@@ -52,7 +57,9 @@ public class StoreImpl implements IStore {
 
     @Override
     public List<Long> get(String id) {
-        List<Long> list = jdbcTemplate.queryForList("select v from jnumberserver_store where id='" + id + "' limit 1", Long.class);
+        Object[] obj = new Object[]{id};
+        String sql = "select v from jnumberserver_store where id=? limit 1";
+        List<Long> list = jdbcTemplate.queryForList(sql, obj, Long.class);
         return list;
     }
 
@@ -60,12 +67,15 @@ public class StoreImpl implements IStore {
     @Transactional
     public List<Long> increment(String id, Integer step) {
         List<Long> list = null;
-
-        int rows = jdbcTemplate.update("update jnumberserver_store set v=v+" + step + " where id='" + id + "'");
+        Object[] obj = new Object[]{step, id};
+        String sql = "update jnumberserver_store set v=v+? where id=?";
+        int rows = jdbcTemplate.update(sql, obj);
         if (rows == 0)
             return list;
 
-        list = jdbcTemplate.queryForList("select v from jnumberserver_store where id='" + id + "' limit 1", Long.class);
+        Object[] obj1 = new Object[]{id};
+        String sql1 = "select v from jnumberserver_store where id=? limit 1";
+        list = jdbcTemplate.queryForList(sql1, obj1, Long.class);
 
         return list;
     }
@@ -75,12 +85,15 @@ public class StoreImpl implements IStore {
     public List<Long> decrement(String id, Integer step) {
 
         List<Long> list = null;
-
-        int rows = jdbcTemplate.update("update jnumberserver_store set v=v-" + step + " where id='" + id + "'");
+        Object[] obj = new Object[]{step, id};
+        String sql = "update jnumberserver_store set v=v-? where id=?";
+        int rows = jdbcTemplate.update(sql, obj);
         if (rows == 0)
             return list;
 
-        list = jdbcTemplate.queryForList("select v from jnumberserver_store where id='" + id + "' limit 1", Long.class);
+        Object[] obj1 = new Object[]{id};
+        String sql1 = "select v from jnumberserver_store where id=? limit 1";
+        list = jdbcTemplate.queryForList(sql1, obj1, Long.class);
 
         return list;
     }
@@ -88,37 +101,44 @@ public class StoreImpl implements IStore {
     @Override
     @Transactional
     public long incrementOrAdd(String id, Integer step, Long defaultValue) {
-
-
-        int rows = jdbcTemplate.update("update jnumberserver_store set v=v+" + step + " where id='" + id + "'");
+        Object[] obj = new Object[]{step, id};
+        String sql = "update jnumberserver_store set v=v+? where id=?";
+        int rows = jdbcTemplate.update(sql, obj);
         if (rows == 0) {
             rows = jdbcTemplate.update("insert into jnumberserver_store(id,v,createtime) values(?,?,?)",
                     id, defaultValue, new Date());
         }
-
-        List<Long> list = jdbcTemplate.queryForList("select v from jnumberserver_store where id='" + id + "' limit 1", Long.class);
+        obj = new Object[]{id};
+        sql = "select v from jnumberserver_store where id=? limit 1";
+        List<Long> list = jdbcTemplate.queryForList(sql, obj, Long.class);
         return list.get(0);
     }
 
     @Override
     @Transactional
     public long decrementOrAdd(String id, Integer step, Long defaultValue) {
-        int rows = jdbcTemplate.update("update jnumberserver_store set v=v-" + step + " where id='" + id + "'");
+        Object[] obj = new Object[]{step, id};
+        String sql = "update jnumberserver_store set v=v-? where id=?";
+        int rows = jdbcTemplate.update(sql, obj);
         if (rows == 0) {
             rows = jdbcTemplate.update("insert into jnumberserver_store(id,v,createtime) values(?,?,?)",
                     id, defaultValue, new Date());
         }
-
-        List<Long> list = jdbcTemplate.queryForList("select v from jnumberserver_store where id='" + id + "' limit 1 ", Long.class);
+        obj = new Object[]{id};
+        sql = "select v from jnumberserver_store where id=? limit 1 ";
+        List<Long> list = jdbcTemplate.queryForList(sql, obj, Long.class);
         return list.get(0);
     }
 
     @Override
     @Transactional
     public Long getOrAdd(String id, Long defaultValue) {
-        jdbcTemplate.update("update jnumberserver_store set v=v+0 where id='" + id + "'");
+        Object[] obj = new Object[]{id};
+        String sql = "update jnumberserver_store set v=v+0 where id=?";
+        jdbcTemplate.update(sql, obj);
 
-        List<Long> list = jdbcTemplate.queryForList("select v from jnumberserver_store where id='" + id + "' limit 1", Long.class);
+        String sql1 = "select v from jnumberserver_store where id=? limit 1";
+        List<Long> list = jdbcTemplate.queryForList(sql1, obj, Long.class);
 
         if (list == null || list.size() == 0) {
             jdbcTemplate.update("insert into jnumberserver_store(id,v,createtime) values(?,?,?)",
