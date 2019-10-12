4eeda20452adc320e87b7df403040e532de9dda7
xiaobogit <lxb@aooshi.org>
2019-10-12 18:01:01 +0800

    sql

--- src/main/resources/NumberServer.sql
+++ src/main/resources/NumberServer.sql
@@ -1,24 +1,7 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : 11:3306
-Source Server Version : 50629
-Source Host           : 192.168.199.11:3306
-Source Database       : aooshi
-
-Target Server Type    : MYSQL
-Target Server Version : 50629
-File Encoding         : 65001
-
-Date: 2019-10-12 17:36:22
-*/
-
-SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
 -- Table structure for jnumberserver_store
 -- ----------------------------
-DROP TABLE IF EXISTS `jnumberserver_store`;
 CREATE TABLE `jnumberserver_store` (
   `id` varchar(255) NOT NULL,
   `v` bigint(20) NOT NULL DEFAULT '0',
@@ -29,7 +12,6 @@ CREATE TABLE `jnumberserver_store` (
 -- ----------------------------
 -- Table structure for jnumberserver_user
 -- ----------------------------
-DROP TABLE IF EXISTS `jnumberserver_user`;
 CREATE TABLE `jnumberserver_user` (
   `uid` int(11) NOT NULL,
   `pwd` char(32) NOT NULL DEFAULT '0',
@@ -37,3 +19,12 @@ CREATE TABLE `jnumberserver_user` (
   `createtime` datetime DEFAULT NULL,
   PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='数值服务器访问授权';
+
+
+
+
+INSERT INTO `jnumberserver_user` set uid=1,pwd='yAYX4yA2hERjmyt352XXzWafcmWpCWYS',state=0,createtime=now();
+INSERT INTO `jnumberserver_store` SET id="1",v=111111,createtime=now();
+
+
+
